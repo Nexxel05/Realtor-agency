007d3a3b0d2534b5461adac5dc2e31a9e947c060
Kostyantyn Chernyk <chernykko@gmail.com>
2023-02-16 15:02:40 +0000

    Register Realtor on admin site

--- advertisement/admin.py
+++ advertisement/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from advertisement.models import Realtor
+
+
+@admin.register(Realtor)
+class RealtorAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience", "phone_number", "date_registered")
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("years_of_experience", "phone_number")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Personal info", {"fields": ("first_name", "last_name", "email")}),
+        ("Additional info", {"fields": ("years_of_experience", "phone_number")},)
+    )
