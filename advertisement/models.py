f5343c244cf1e713bfd98f61bf7cd54fb65bbd2a
Kostyantyn Chernyk <chernykko@gmail.com>
2023-02-16 14:20:06 +0000

    Add Realtor model

--- advertisement/models.py
+++ advertisement/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Realtor(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    class Meta:
+        verbose_name = "realtor"
+
+    def str(self):
+        return f"{self.first_name} {self.last_name} ({self.username})"
