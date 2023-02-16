2da0d38f07a986767da7f2829071edaced34de10
Kostyantyn Chernyk <chernykko@gmail.com>
2023-02-16 16:35:52 +0000

    Create index view and configure templates path in settings.py

--- advertisement/views.py
+++ advertisement/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from advertisement.models import Realtor, Advertisement
+
+
+def index(request):
+    num_of_realtors = Realtor.objects.count()
+    num_of_advertisements = Advertisement.objects.count()
+
+    context = {
+        "num_of_realtors": num_of_realtors,
+        "num_of_advertisements": num_of_advertisements
+    }
+    return render(request, "advertisement/index.html", context)
+
