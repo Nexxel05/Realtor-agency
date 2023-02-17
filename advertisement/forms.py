68edbc8f0fa45c9a1e69d0cbebeb0f9a844ab7c5
Kostyantyn Chernyk <chernykko@gmail.com>
2023-02-17 14:49:58 +0000

    Add forms.py to project and create realtor creation form

---
+++ advertisement/forms.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.forms import UserCreationForm
+
+from advertisement.models import Realtor
+
+
+class RealtorCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Realtor
+        fields = UserCreationForm.Meta.fields + (
+            "first_name",
+            "last_name",
+            "email",
+            "years_of_experience",
+            "phone_number"
+        )
