7666dd3c0cd1394be8da0a92b195530cde0f3503
Kostyantyn Chernyk <chernykko@gmail.com>
2023-02-21 14:00:02 +0000

    Alter apartment, description, price and total_area in Advertisement model

--- advertisement/models.py
+++ advertisement/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator
 from django.db import models
 from django.urls import reverse
 
@@ -45,15 +46,30 @@ class City(models.Model):
 
 
 class Advertisement(models.Model):
-    city = models.ForeignKey(City, on_delete=models.CASCADE, related_name="advertisements")
+    city = models.ForeignKey(
+        City,
+        on_delete=models.CASCADE,
+        related_name="advertisements"
+    )
     street = models.CharField(max_length=63)
-    house = models.IntegerField()
-    apartment = models.CharField(max_length=63, null=True, blank=True)
-    description = models.TextField(blank=True)
-    total_area = models.CharField(max_length=10)
-    price = models.DecimalField(max_digits=12, decimal_places=2)
-    property = models.ForeignKey(Property, on_delete=models.CASCADE, related_name="advertisements")
-    realtors = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="advertisements")
+    house = models.PositiveSmallIntegerField()
+    apartment = models.PositiveSmallIntegerField(null=True, blank=True)
+    description = models.TextField(blank=True, null=True)
+    total_area = models.PositiveSmallIntegerField(null=True, blank=True)
+    price = models.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+        validators=[MinValueValidator(0, "Price can not be negative")]
+    )
+    property = models.ForeignKey(
+        Property,
+        on_delete=models.CASCADE,
+        related_name="advertisements"
+    )
+    realtors = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="advertisements"
+    )
     sold = models.BooleanField(default=False)
 
     def __str__(self):
